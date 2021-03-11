d7428cfc44fc5aac907f95be8317789c9afcb42a
AquilesD <kilesax@gmail.com>
2021-03-11 11:50:49 -0300

    Finalizando template

--- clientes-app/src/app/template/template.module.ts
+++ clientes-app/src/app/template/template.module.ts
@@ -4,12 +4,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
-
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
+  declarations: [
+    NavbarComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [NavbarComponent,SidebarComponent]
+  exports: [
+    NavbarComponent,
+    SidebarComponent
+  ]
 })
 export class TemplateModule { }
