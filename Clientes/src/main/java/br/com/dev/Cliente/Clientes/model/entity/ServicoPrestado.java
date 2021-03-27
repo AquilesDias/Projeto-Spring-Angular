d096957a7110f03d866f0c320b771326891729c7
AquilesD <kilesax@gmail.com>
2021-03-27 12:59:39 -0300

    salvando servico prestado

--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/model/entity/ServicoPrestado.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/model/entity/ServicoPrestado.java
@@ -1,6 +1,7 @@
 package br.com.dev.Cliente.Clientes.model.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -26,5 +27,6 @@ public class ServicoPrestado {
     private BigDecimal valor;
 
     @Column
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate data;
 }
--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/rest/ServicoPrestadoController.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/rest/ServicoPrestadoController.java
@@ -5,31 +5,32 @@ import br.com.dev.Cliente.Clientes.model.entity.ServicoPrestado;
 import br.com.dev.Cliente.Clientes.model.repository.ClienteRepository;
 import br.com.dev.Cliente.Clientes.model.repository.ServicoPrestadoRepository;
 import br.com.dev.Cliente.Clientes.rest.dto.ServicoPrestadoDTO;
+import br.com.dev.Cliente.Clientes.util.BigDecimalConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-@RequestMapping
+
 @RequiredArgsConstructor
-@RestController("api/servico-prestado")
+@RestController
+@RequestMapping("/api/servicos-prestados")
 public class ServicoPrestadoController {
 
     private final ClienteRepository clienteRepository;
     private final ServicoPrestadoRepository servicoPrestadoRepository;
+    private final BigDecimalConverter bigDecimalConverter;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto){
 
         LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
         Integer idCliente = dto.getIdCliente();
+
         Cliente cliente = clienteRepository.findById(idCliente)
                 .orElseThrow(()
                         -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
@@ -39,7 +40,8 @@ public class ServicoPrestadoController {
         servicoPrestado.setDescricao(dto.getDescricao());
         servicoPrestado.setData(data);
         servicoPrestado.setCliente(cliente);
-        servicoPrestado.setValor();
-        return servicoPrestado;
+        servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
+
+        return servicoPrestadoRepository.save(servicoPrestado);
     }
 }
