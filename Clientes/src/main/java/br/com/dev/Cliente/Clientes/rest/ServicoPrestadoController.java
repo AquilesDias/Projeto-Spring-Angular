62e583fb7997db0f6adcea734721e31a7503d74f
AquilesD <kilesax@gmail.com>
2021-03-29 18:40:49 -0300

    Configurando o CORS

--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/rest/ServicoPrestadoController.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/rest/ServicoPrestadoController.java
@@ -19,7 +19,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/servicos-prestados")
-@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoController {
 
     private final ClienteRepository clienteRepository;
