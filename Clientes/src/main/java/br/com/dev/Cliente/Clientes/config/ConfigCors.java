ecde2e954cddf7ee6f98d7ed8da628651b092cc0
AquilesD <kilesax@gmail.com>
2021-03-29 20:12:22 -0300

    Configurando Cors

--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/ConfigCors.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/ConfigCors.java
@@ -4,14 +4,10 @@ package br.com.dev.Cliente.Clientes.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class ConfigCors {
@@ -28,24 +24,4 @@ public class ConfigCors {
         return bean;
     }
 
-
-//    @Bean
-//    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
-//        List<String> all = Arrays.asList("*");
-//
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowedOrigins(all);
-//        corsConfiguration.setAllowedHeaders(all);
-//        corsConfiguration.setAllowedMethods(all);
-//        corsConfiguration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//
-//        CorsFilter corsFilter = new CorsFilter(source);
-//        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
-//        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//
-//        return filter;
-//    }
 }
