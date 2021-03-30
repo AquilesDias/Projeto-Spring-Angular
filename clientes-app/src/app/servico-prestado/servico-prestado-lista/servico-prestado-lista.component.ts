3a53aa851fee3b5f812f391a129e5ef83edb0d9d
AquilesD <kilesax@gmail.com>
2021-03-30 07:11:06 -0300

    implementando a pesquisa de servicos prestados

--- clientes-app/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -7,19 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoListaComponent implements OnInit {
 
-  nome: String;
+  nome: string;
   mes: number;
   meses: number[];
 
-  constructor() { 
-    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12]
+  lista: ServicoPrestadoBusca[];
+
+  constructor(
+    private service: ServicoPrestadoService
+  ) { 
+    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12] 
   }
 
   ngOnInit(): void {
   }
 
   consultar(){
-    console.log(this.nome, this.mes);
+      this.service.buscar(this.nome, this.mes)
+      .subscribe(response => this.lista = response)
   }
 
 }
