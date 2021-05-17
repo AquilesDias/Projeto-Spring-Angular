58aca275faf3a963dd87bcbf004aeac43edc7ce5
AquilesD <kilesax@gmail.com>
2021-05-17 17:47:38 -0300

    Criação do formulario de Login

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http'
-
 import { AppRoutingModule } from  './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { TemplateModule } from './template/template.module';
-import { HomeComponent } from './home/home.component'
 import { ClientesModule } from './clientes/clientes.module';
+
+import { FormsModule} from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
 import { ClientesService } from './clientes.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
     
   ],
   imports: [
@@ -24,7 +27,8 @@ import { ServicoPrestadoService } from './servico-prestado.service';
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
-    ServicoPrestadoModule
+    ServicoPrestadoModule,
+    FormsModule
   ],
   providers: [ClientesService, ServicoPrestadoService],
   bootstrap: [AppComponent]
