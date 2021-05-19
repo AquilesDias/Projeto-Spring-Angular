6028a63be1990b755e4ea6c57a393ad27dabba50
AquilesD <kilesax@gmail.com>
2021-05-19 15:39:04 -0300

    Modelanndo a entidade Usuario

---
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/model/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.dev.Cliente.Clientes.model.repository;
+
+import br.com.dev.Cliente.Clientes.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+}
