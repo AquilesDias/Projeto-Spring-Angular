b09b1eaaaea7bd65149b6926bfdfb8f345b3e24f
AquilesD <kilesax@gmail.com>
2021-05-21 08:24:35 -0300

    Configurando o Resource Server

--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/ResourceServerConfig.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/ResourceServerConfig.java
@@ -1,4 +1,18 @@
 package br.com.dev.Cliente.Clientes.config;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/api/usuarios").permitAll()
+                    .antMatchers("/api/servicos-prestados").authenticated()
+                .anyRequest().denyAll();
+    }
 }
