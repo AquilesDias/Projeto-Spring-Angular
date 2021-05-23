101ddfdb55b43247042b3d57ac99176906180d3c
AquilesD <kilesax@gmail.com>
2021-05-23 01:34:09 -0300

    Configs

--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/AuthorizationServerConfig.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package br.com.dev.Cliente.Clientes.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -17,6 +18,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Bean
     public TokenStore tokenStore(){
         return new InMemoryTokenStore();
     }
--- Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/ResourceServerConfig.java
+++ Clientes/src/main/java/br/com/dev/Cliente/Clientes/config/ResourceServerConfig.java
@@ -2,9 +2,11 @@ package br.com.dev.Cliente.Clientes.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
@@ -12,7 +14,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/api/usuarios").permitAll()
-                    .antMatchers("/api/servicos-prestados").authenticated()
+                    .antMatchers("/api/clientes/**","/api/servicos-prestados/**").authenticated()
                 .anyRequest().denyAll();
     }
 }
